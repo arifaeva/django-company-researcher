c153f888994fa0ee6b7049c27101215b54a09a5c
arifaeva <arifa.eva.ixa@gmail.com>
2025-04-23 23:42:15 +0700

    improve UX

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from .consumer import NotificationConsumer
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("researcher.urls"))
+]
+
+websocket_urlpatterns = [
+    path("ws/notifications/", NotificationConsumer.as_asgi())
 ]
--- researcher/tasks.py
+++ researcher/tasks.py
@@ -1,13 +1,21 @@
 from researcher.methods import generate_business_profile, generate_queries, research
 from huey.contrib.djhuey import task
+from core.methods import send_notification
 
 @task()
 def process_research(company_name):
     queries = []
 
+    send_notification("notification", "Generating financial queries")
     financial_queries = generate_queries("Finance", company_name)
+
+    send_notification("notification", "Generating leadership queries")
     leadership_queries = generate_queries("Leadership", company_name)
+
+    send_notification("notification", "Generating operations queries")
     operations_queries = generate_queries("Operations", company_name)
+
+    send_notification("notification", "Generating corporate history queries")
     corporate_history_queries = generate_queries("Corporate History", company_name)
 
     queries.extend(financial_queries.get("queries"))
@@ -18,9 +26,13 @@ def process_research(company_name):
     context = ""
 
     for query in queries:
+        send_notification("notification", f"Researching about {query}")
         response = research(query)
         context += f"Query : {query} \nResponse : {response} \n\n"
 
+    send_notification("notification", "Generating business profile")
     business_profile = generate_business_profile(context)
+
+    send_notification("final_result", business_profile)
         
     print(business_profile)
